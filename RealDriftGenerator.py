4bb86c5e6c390e5902613d4efd1e5ee78fa3eeca
sniperrifle71 <99159687+sniperrifle71@users.noreply.github.com>
2024-04-23 10:24:14 +0800

    upload sample

--- RealDriftGenerator.py
+++ RealDriftGenerator.py
@@ -57,12 +57,16 @@ class RealDriftGenerator:
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("./electricity-normalized.csv",
-                     usecols=["nswprice", "nswdemand", "vicprice", "vicdemand", "transfer", "class"],
-                     nrows=1000)
 
-    df["class"] = df["class"].apply(lambda element: 1 if element == "UP" else 0)
-    drift_dict = {700: (100, "middle")}
-    drift = RealDriftGenerator(df=df)
-    reverse = drift.reverseSlice(drift_dict=drift_dict)
+    df = pd.read_csv("../seattle-weather.csv", nrows=1000,
+                     usecols=["precipitation", "temp_max", "temp_min", "wind", "weather"])
+    class_dict = {"drizzle": 0, "rain": 1, "sun": 2, "snow": 3, "fog": 4}
+    df["weather"] = df["weather"].apply(lambda x: class_dict[x])
+
+    feature_dim = df.shape[1] - 1
+    class_dim = int(df.iloc[:, feature_dim].max()) + 1
+    stream = RealDriftGenerator(df)
+    df = stream.reverseSlice(drift_dict={700: (100, "middle")})
+    df.to_csv("./weather_p700_w100_l1000.csv")
+
 
